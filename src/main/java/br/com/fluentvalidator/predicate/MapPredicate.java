7b72b722c59e417da0f934c19d9e1a15606c412c
Marcos Tischer Vallim <tischer@gmail.com>
2021-04-12 23:21:54 -0300

    Improve tests

--- src/main/java/br/com/fluentvalidator/predicate/MapPredicate.java
+++ src/main/java/br/com/fluentvalidator/predicate/MapPredicate.java
@@ -1,9 +1,5 @@
 package br.com.fluentvalidator.predicate;
 
-import static br.com.fluentvalidator.predicate.ComparablePredicate.between;
-import static br.com.fluentvalidator.predicate.ComparablePredicate.betweenInclusive;
-import static br.com.fluentvalidator.predicate.ComparablePredicate.equalTo;
-import static br.com.fluentvalidator.predicate.LogicalPredicate.is;
 import static br.com.fluentvalidator.predicate.LogicalPredicate.not;
 import static br.com.fluentvalidator.predicate.ObjectPredicate.nullValue;
 
@@ -17,76 +13,43 @@ public final class MapPredicate {
     super();
   }
   
+  public static <K, V, T extends Map<K, V>> Predicate<T> mapGet(final K key, final Predicate<V> predicate) {
+    return PredicateBuilder.<T>from(not(nullValue()))
+      .and(obj -> not(nullValue()).test(key))
+      .and(obj -> not(nullValue()).test(predicate))
+      .and(obj -> predicate.test(obj.get(key)));
+  }
+  
+  public static <K, V, T extends Map<K, V>> Predicate<T> mapGet(final Function<T, K> key, final Predicate<V> predicate) {
+    return PredicateBuilder.<T>from(not(nullValue()))
+      .and(obj -> not(nullValue()).test(key))
+      .and(obj -> not(nullValue()).test(key.apply(obj)))
+      .and(obj -> not(nullValue()).test(predicate))
+      .and(obj -> predicate.test(obj.get(key.apply(obj))));
+  }
+  
   public static <K, V, T extends Map<K, V>> Predicate<T> containsKey(final K key) {
-    return PredicateBuilder.<T>from(is(nullValue()))
+    return PredicateBuilder.<T>from(not(nullValue()))
+      .and(obj -> not(nullValue()).test(key))
       .and(obj -> obj.containsKey(key));
   }
 
   public static <K, V, T extends Map<K, V>> Predicate<T> containsKey(final Function<T, K> key) {
-    return PredicateBuilder.<T>from(is(nullValue()))
-      .and(obj -> obj.containsKey(key));
+    return PredicateBuilder.<T>from(not(nullValue()))
+      .and(obj -> not(nullValue()).test(key))
+      .and(obj -> not(nullValue()).test(key.apply(obj)))
+      .and(obj -> obj.containsKey(key.apply(obj)));
   }
   
   public static <K, V, T extends Map<K, V>> Predicate<T> containsValue(final V value) {
-    return PredicateBuilder.<T>from(is(nullValue()))
+    return PredicateBuilder.<T>from(not(nullValue()))
       .and(obj -> obj.containsValue(value));
   }
   
   public static <K, V, T extends Map<K, V>> Predicate<T> containsValue(final Function<T, V> value) {
-    return PredicateBuilder.<T>from(is(nullValue()))
-      .and(obj -> obj.containsValue(value));
-  }
-
-  public static <K, V, T extends Map<K, V>> Predicate<T> empty() {
-    return PredicateBuilder.<T>from(is(nullValue()))
-      .or(Map::isEmpty);
-  }
-
-  public static <K, V, T> Predicate<T> empty(final Function<T, Map<K, V>> source) {
-    return PredicateBuilder.<T>from(is(nullValue()))
-      .or(obj -> is(nullValue()).test(source.apply(obj)))
-      .or(obj -> source.apply(obj).isEmpty());
-  }
-
-  public static <T, K, V> Predicate<T> hasSize(final Function<T, Map<K, V>> source, final Function<T, Integer> size) {
     return PredicateBuilder.<T>from(not(nullValue()))
-      .and(obj -> not(nullValue(size)).test(obj))
-      .and(obj -> not(nullValue(source)).test(obj))
-      .and(obj -> equalTo(size.apply(obj)).test(source.apply(obj).size()));
+      .and(obj -> not(nullValue()).test(value))
+      .and(obj -> obj.containsValue(value.apply(obj)));
   }
-
-  public static <K, V, T> Predicate<T> hasSize(final Function<T, Map<K, V>> source, final Integer size) {
-    return PredicateBuilder.<T>from(not(nullValue()))
-      .and(obj -> not(nullValue(source)).test(obj))
-      .and(obj -> equalTo(size).test(source.apply(obj).size()));
-  }
-
-  public static <K, V, T extends Map<K, V>> Predicate<T> hasSize(final Integer size) {
-    return PredicateBuilder.<T>from(not(nullValue()))
-      .and(obj -> equalTo(size).test(obj.size()));
-  }
-
-  public static <K, V, T> Predicate<T> hasSizeBetween(final Function<T, Map<K, V>> source, final Integer min, final Integer max) {
-    return PredicateBuilder.<T>from(not(nullValue()))
-      .and(obj -> not(nullValue(source)).test(obj))
-      .and(obj -> between(min, max).test(source.apply(obj).size()));
-  }
-
-  public static <K, V, T extends Map<K, V>> Predicate<T> hasSizeBetween(final Integer min, final Integer max) {
-    return PredicateBuilder.<T>from(not(nullValue()))
-      .and(between(Map::size, min, max));
-  }
-
-  public static <K, V, T> Predicate<T> hasSizeBetweenInclusive(final Function<T, Map<K, V>> source, final Integer min, final Integer max) {
-    return PredicateBuilder.<T>from(not(nullValue()))
-      .and(obj -> not(nullValue(source)).test(obj))
-      .and(obj -> betweenInclusive(min, max).test(source.apply(obj).size()));
-  }
-
-  public static <K, V, T extends Map<K, V>> Predicate<T> hasSizeBetweenInclusive(final Integer min, final Integer max) {
-    return PredicateBuilder.<T>from(not(nullValue()))
-      .and(betweenInclusive(Map::size, min, max));
-  }
-
 
 }
